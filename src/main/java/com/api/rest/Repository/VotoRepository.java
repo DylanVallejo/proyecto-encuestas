3f66328ffbcc906ad242e9e2376e761bda43fd7d
DylanVallejo <dylan1811vallejo@gmail.com>
2024-01-11 23:48:27 -0500

    - creacion de query y metodo para encontrar una encuesta con sus votos

--- src/main/java/com/api/rest/Repository/VotoRepository.java
+++ src/main/java/com/api/rest/Repository/VotoRepository.java
@@ -3,11 +3,13 @@ package com.api.rest.Repository;
 import com.api.rest.Model.Encuesta;
 import com.api.rest.Model.Voto;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface VotoRepository extends JpaRepository<Voto, Long>{
 
-
+    @Query(value = "select v.* from Opcion o , Voto v where o.encuesta_id = ?1 and v.opcion_id = o.opcion_id", nativeQuery = true)
+    public Iterable<Voto> findByEncuesta(Long encuestaId);
 
 }
